ef907d34522b13d2a8a5a655353b74c1d6c306dc
danigb <danigb@gmail.com>
2014-08-17 23:35:00 +0200

    Deployment

--- config.rb
+++ config.rb
@@ -66,3 +66,14 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host   = 'bookcamping.cc'
+  deploy.path   = '/home/deployer/bucolicas/'
+  deploy.user  = 'deployer' # no default
+  # deploy.port  = 5309 # ssh port, default: 22
+  # deploy.clean = true # remove orphaned files on remote host, default: false
+  # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
+end
